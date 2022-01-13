43612c6b8f2a1f6b298d74c98aac8886ea2cd377
nic0mp <nico.mp215@gmail.com>
2022-01-12 21:11:35 -0600

    added react-typical feature

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Typical from 'react-typical'
 
 export default function Profile() {
     return (
@@ -6,21 +7,50 @@ export default function Profile() {
             <div className="profile-parent">
                 <div className="profile-details">
                     <div className="colz">
-                        <a href="#">
+                        <a href="https://github.com/nic0mp">
                             <i className="fa fa-github-square"></i>
                         </a>
-                        <a href="#">
+                        <a href="https://twitter.com/nic0mp">
                             <i className="fa fa-twitter-square"></i>
                         </a>
-                        <a href="#">
+                        <a href="https://www.instagram.com/nic0mp/">
                             <i className="fa fa-instagram"></i>
                         </a>
-                        <a href="#">
+                        <a href="https://www.linkedin.com/in/nico-mp/">
                             <i className="fa fa-linkedin-square"></i>
                         </a>
+                    </div>
+
+                    <div className="profile-details-name">
+                        <span className="primary-text">
+                            {" "}
+                            Hello, I'M <span className="highlighted-text">Nico</span>
+                        </span>
+                    </div>
+                    <div className="profile-details-role">
+                        <span className="primary-text">
+                            {" "}
+                            <h1>
+                                {" "}
+                                <Typical 
+                                loop={Infinity}
+                                steps={[
+                                    "Enthusiastic 🏈",
+                                    1000,
+                                    "Full Stack",
+                                    1000,
+                                    "Enthusiastic",
+                                    1000,
+                                    "Enthusiastic",
+                                    1000,
+                                ]}
+                                />
+                            </h1>
+                            
+                        </span>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
